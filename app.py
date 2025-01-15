91298984930c9809057c7957df9642b23bfe8bcd
Avichay Har-Tuv <mydadisalive@gmail.com>
2025-01-15 13:22:08 +0200

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ def index():
 #        gifs = [row[0] for row in cursor.fetchall()]
 #        db.close()
 
-        images = [
+        gifs = [
             "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F0.gif?alt=media&token=0fff4b31-b3d8-44fb-be39-723f040e57fb",
             "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F1.gif?alt=media&token=2328c855-572f-4a10-af8c-23a6e1db574c",
             "https://firebasestorage.googleapis.com/v0/b/docker-curriculum.appspot.com/o/catnip%2F10.gif?alt=media&token=647fd422-c8d1-4879-af3e-fea695da79b2",
